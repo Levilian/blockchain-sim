c39fc847eeaba748c0e04192e38af623c7fd753f
Matthew Leeds <mwl458@gmail.com>
2016-04-21 17:58:52 -0500

    Implement transaction propagation between nodes using simlib's event scheduling functions.

--- src/Node.cpp
+++ src/Node.cpp
@@ -1,9 +1,15 @@
 
 #include "Node.h"
+#include "simlib.h"
+#include "blockchain-sim-defs.h"
 
-Node::Node(Type type) {
+Node::Node(Type type, unsigned int node_no) {
     this->_type = type;
+    this->_node_no = node_no;
     this->_adjList = new vector<Link*>;
+    this->_known_transactions = new vector<Transaction*>;
+    this->_known_blocks = new vector<Block*>;
+    this->_in_transit_tx_nos = new vector<unsigned int>;
 }
 
 Node::~Node() {
@@ -11,11 +17,75 @@ Node::~Node() {
         delete *it;
     }
     delete this->_adjList;
+    for (vector<Transaction*>::iterator it = this->_known_transactions->begin(); it != this->_known_transactions->end(); ++it) {
+        delete *it;
+    }
+    delete this->_known_transactions;
+    for (vector<Block*>::iterator it = this->_known_blocks->begin(); it != this->_known_blocks->end(); ++it) {
+        delete *it;
+    }
+    delete this->_known_blocks;
+    delete this->_in_transit_tx_nos;
 }
 
 void Node::add_link(Node* other_node, float speed) {
     Link* new_link = new Link(other_node, speed);
     this->_adjList->push_back(new_link);
+}
+
+bool Node::aware_of(Transaction* tx) {
+    bool already_known = false;
+    for (vector<Transaction*>::iterator it = this->_known_transactions->begin(); it != this->_known_transactions->end(); ++it) {
+        if ((*it)->get_tx_no() == tx->get_tx_no()) already_known = true;
+    }
+    for (vector<unsigned int>::iterator it = this->_in_transit_tx_nos->begin(); it != this->_in_transit_tx_nos->end(); ++it) {
+        if (*it == tx->get_tx_no()) already_known = true;
+    }
+    return already_known;
+}
+
+bool Node::aware_of(Block* b) {
+    bool already_known = false;
+    for (vector<Block*>::iterator it = this->_known_blocks->begin(); it != this->_known_blocks->end(); ++it) {
+        if ((*it)->get_block_no() == b->get_block_no()) already_known = true;
+    }
+    return already_known;
+}
+
+bool Node::linked_to(unsigned int node_no) {
+    bool linked = false;
+    for (vector<Link*>::iterator it = this->_adjList->begin(); it != this->_adjList->end(); ++it) {
+        if ((*it)->get_other_node()->get_node_no() == node_no) linked = true;
+    }
+    return linked;
+}
+
+void Node::in_transit_tx(unsigned int tx_no) {
+    this->_in_transit_tx_nos->push_back(tx_no);
+}
+
+void Node::broadcast_transaction(Transaction* tx) {
+    // add it to our list of transactions
+    this->_known_transactions->push_back(tx);
+
+    // schedule events for neighboring nodes to be aware of it
+    for (vector<Link*>::iterator it = this->_adjList->begin(); it != this->_adjList->end(); ++it) {
+        if (!((*it)->get_other_node()->aware_of(tx))) {
+            printf("broadcasting tx %d from node %d to node %d\n", tx->get_tx_no(), this->get_node_no(), (*it)->get_other_node()->get_node_no());
+            transfer[3] = tx->get_tx_no();
+            transfer[4] = tx->get_tx_fee();
+            transfer[5] = (*it)->get_other_node()->get_node_no();
+            event_schedule(sim_time + (*it)->get_speed(), EVENT_TX_RELAY);
+            // record that it's in transit so it isn't broadcast again before it arrives
+            (*it)->get_other_node()->in_transit_tx(tx->get_tx_no());
+        }
+    }
+}
+
+void Node::broadcast_block(Block* b) {
+    //TODO check if we already know about the block
+    //TODO remove tx from known_transactions
+    //TODO let other nodes know about it
 }
 
 ostream& operator<<(ostream& os, const Node& n) {
--- src/Node.h
+++ src/Node.h
@@ -2,8 +2,6 @@
 // This class represents a server running blockchain software 
 // that relays transactions or mines blocks.
 
-#pragma once
-
 #include <iostream>
 #include <vector>
 
@@ -19,21 +17,62 @@ typedef struct Link {
             _other_node = other_node;
             _speed = speed;
         }
+        Node* get_other_node() { return _other_node; }
+        float get_speed() { return _speed; }
     private:
         Node* _other_node;
         float _speed;
 } Link;
 
+typedef struct Transaction {
+    public:
+        Transaction(unsigned int tx_no, float tx_fee) {
+            _tx_no = tx_no;
+            _tx_fee = tx_fee;
+        }
+        unsigned int get_tx_no() { return _tx_no; }
+        float get_tx_fee() { return _tx_fee; }
+    private:
+        unsigned int _tx_no;
+        float _tx_fee;
+} Transaction;
+
+typedef struct Block {
+    public:
+        Block(unsigned int block_no, vector<Transaction*>* transactions) {
+            _block_no = block_no;
+            _transactions = transactions;
+        }
+        unsigned int get_block_no() { return _block_no; }
+        vector<Transaction*>* get_transactions() { return _transactions; }
+    private:
+        unsigned int _block_no;
+        vector<Transaction*>* _transactions;
+} Block;
+
 class Node {
     public:
-        Node(Type type);
+        Node(Type type, unsigned int node_no);
         ~Node();
         Type GetType() const { return _type; }
         void add_link(Node* otherNode, float speed);
         unsigned int get_num_links() { return _adjList->size(); }
+        void in_transit_tx(unsigned int tx_no);
+        void broadcast_transaction(Transaction* tx);
+        void broadcast_block(Block* b);
+        unsigned int get_node_no() { return _node_no; }
+        vector<Transaction*>* get_known_transactions() { return _known_transactions; }
+        vector<Block*>* get_known_blocks() { return _known_blocks; }
+        bool aware_of(Transaction* tx);
+        bool aware_of(Block* b);
+        bool linked_to(unsigned int node_no);
     private:
+        friend ostream& operator<<(ostream& os, const Node& n);
         Type _type;
         vector<Link*>* _adjList;
-        friend ostream& operator<<(ostream& os, const Node& n);
+        vector<Transaction*>* _known_transactions;
+        vector<Block*>* _known_blocks;
+        vector<unsigned int>* _in_transit_tx_nos;
+        unsigned int _node_no;
 };
 
